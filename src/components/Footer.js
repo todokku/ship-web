7bed01cc442d0739a3f439921964ecf65acba70d
Victor Truong <me@ifvictr.com>
2018-08-15 11:53:49 -0700

    Add pattern background to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,17 @@ import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome'
 import { Box, Link, Text, colors } from '@hackclub/design-system'
 import React from 'react'
 
-const Base = Box.withComponent('footer')
+const Base = Box.withComponent('footer').extend.attrs({
+    bg: 'snow',
+    color: 'black',
+    p: 4
+})`
+    background-image: url(/pattern.svg);
+    background-size: 20rem;
+`
 
 const Footer = props => (
-    <Base bg="snow" color="black" p={4} {...props}>
+    <Base {...props}>
         <Text align="center" f={3}>
             Made with <FA icon="code" color={colors.info} /> and <FA icon="heart" color={colors.primary} /> by{' '}
             <Link href="https://ifvictr.com" target="_blank">Victor Truong</Link>
