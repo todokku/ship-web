3b59dccf43eebff26743859e92805e66c4f09e55
Victor Truong <me@ifvictr.com>
2018-08-16 21:44:38 -0700

    Add API helper

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export const url = process.env.NODE_ENV === 'production'
+    ? 'https://api.ship.hackclub.com'
+    : 'http://localhost:3000/'
+
+const generateMethod = method => (path, options = {}) => {
+    return axios({
+        baseURL: url,
+        url: path,
+        method,
+        ...options
+    })
+}
+
+const api = {}
+const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+methods.forEach(method => {
+    api[method.toLowerCase()] = generateMethod(method)
+})
+
+export default api
