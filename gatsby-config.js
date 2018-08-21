7da49018a44a7db9d307dfb974bca2b49219dbf4
Victor Truong <me@ifvictr.com>
2018-08-21 11:39:33 -0700

    Add topic page logic
    Page design will be added later

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-create-client-paths',
             options: {
-                prefixes: ['/projects/*']
+                prefixes: ['/projects/*', '/topics/*']
             }
         },
         {
---
+++ src/pages/topics/index.js
@@ -0,0 +1,67 @@
+import React, { Component, Fragment } from 'react'
+import Helmet from 'react-helmet'
+import Footer from 'components/Footer'
+import Header from 'components/Header'
+import LoadingBar from 'components/LoadingBar'
+import NotFound from 'pages/404'
+import api from 'api'
+
+class Topics extends Component {
+    state = {
+        projects: [],
+        status: 'loading',
+        topic: {}
+    }
+
+    componentDidMount() {
+        const pathPortions = this.props.location.pathname
+            .split('/')
+            .filter(v => v)
+        // Path is /topics/:slug
+        if (pathPortions.length > 1) {
+            const slug = pathPortions[pathPortions.length - 1]
+            api.get(`v1/topics/slug/${slug}`)
+                .then(res => {
+                    this.setState({ topic: res.data, status: 'topicLoaded' })
+                    return res.data.id
+                })
+                .then(id => api.get(`v1/topics/${id}/projects`))
+                .then(res => {
+                    this.setState({ projects: res.data, status: 'projectsLoaded' })
+                })
+                .catch(() => {
+                    this.setState({ status: 'error' })
+                })
+        }
+        // Path is just /topics
+        else {
+            this.setState({ status: 'error' })
+        }
+    }
+
+    render() {
+        const { projects, status, topic } = this.state
+        switch (status) {
+            case 'loading':
+                return (
+                    <Fragment>
+                        <Header color="muted" />
+                        <LoadingBar fill />
+                    </Fragment>
+                )
+            case 'topicLoaded':
+            case 'projectsLoaded':
+                return (
+                    <Fragment>
+                        <Helmet title={topic.name} />
+                        <Header color="muted" />
+                        <Footer />
+                    </Fragment>
+                )
+            case 'error':
+                return <NotFound />
+        }
+    }
+}
+
+export default Topics
