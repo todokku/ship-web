1e8c63158deb12c75eeb3ffc842e8638dfc76b99
Victor Truong <me@ifvictr.com>
2018-08-20 21:42:43 -0700

    Add bare search page

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,17 +1,29 @@
+import { Container, Heading } from '@hackclub/design-system'
+import qs from 'qs'
 import React, { Component, Fragment } from 'react'
 import Helmet from 'react-helmet'
 import Header from 'components/Header'
 
 class Search extends Component {
-    componentDidMount() {
+    state = { results: [], query: null }
 
+    componentDidMount() {
+        const { q } = qs.parse(this.props.location.search.slice(1))
+        this.setState({ query: q })
+        // TODO: Call API search endpoint and render results
     }
 
     render() {
+        const { results, query } = this.state
         return (
             <Fragment>
-                <Helmet title="Search term here" />
-                <Header />
+                <Helmet title="Search" />
+                <Header color="muted" />
+                <Container px={[3, null, 4]} pt={[5, null, 6]}>
+                    <Heading.h2>
+                        {results.length} result{results.length !== 1 && 's'} for “{query}”
+                    </Heading.h2>
+                </Container>
             </Fragment>
         )
     }
