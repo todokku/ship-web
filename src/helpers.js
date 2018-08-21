3d3a531131bf571057d9982450419a45ddbdf47d
Victor Truong <me@ifvictr.com>
2018-08-21 15:48:26 -0700

    Fix timeago pluralization

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 export const timeSince = (previous, absoluteDuration = false, current = new Date(), longForm = true) => {
     const msPerSecond = 1000
-    const msPerMinute = 60 * 1000
+    const msPerMinute = msPerSecond * 60
     const msPerHour = msPerMinute * 60
     const msPerDay = msPerHour * 24
     const msPerWeek = msPerDay * 7
@@ -8,29 +8,35 @@ export const timeSince = (previous, absoluteDuration = false, current = new Date
 
     const elapsed = new Date(current) - new Date(previous)
 
+    let now
     let humanizedTime
     if (elapsed < msPerMinute) {
-        humanizedTime = '< 1m'
+        humanizedTime = (longForm ? 'less than a minute' : '< 1m')
     }
     else if (elapsed < msPerHour) {
-        const now = Math.round(elapsed / msPerMinute)
-        humanizedTime = (longForm ? `${now} minutes` : `${now}m`)
+        now = Math.round(elapsed / msPerMinute)
+        humanizedTime = (longForm ? `${now} minute` : `${now}m`)
     }
     else if (elapsed < msPerDay) {
-        const now = Math.round(elapsed / msPerHour)
-        humanizedTime = (longForm ? `${now} hours` : `${now}h`)
+        now = Math.round(elapsed / msPerHour)
+        humanizedTime = (longForm ? `${now} hour` : `${now}h`)
     }
     else if (elapsed < msPerWeek) {
-        const now = Math.round(elapsed / msPerDay)
-        humanizedTime = (longForm ? `${now} days` : `${now}d`)
+        now = Math.round(elapsed / msPerDay)
+        humanizedTime = (longForm ? `${now} day` : `${now}d`)
     }
     else if (elapsed < msPerYear) {
-        const now = Math.round(elapsed / msPerWeek)
-        humanizedTime = (longForm ? `${now} weeks` : `${now}w`)
+        now = Math.round(elapsed / msPerWeek)
+        humanizedTime = (longForm ? `${now} week` : `${now}w`)
     }
     else {
-        const now = Math.round(elapsed / msPerYear)
-        humanizedTime = (longForm ? `${now} years` : `${now}y`)
+        now = Math.round(elapsed / msPerYear)
+        humanizedTime = (longForm ? `${now} year` : `${now}y`)
+    }
+
+    // Pluralize result
+    if (longForm && now > 1) {
+        humanizedTime += 's'
     }
 
     if (absoluteDuration) {
