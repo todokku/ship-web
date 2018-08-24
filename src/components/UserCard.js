d7bc79c6ed777b83a48c439494db592b3754f470
Victor Truong <me@ifvictr.com>
2018-08-24 15:54:05 -0700

    Use props to pass data into UserCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,4 +1,4 @@
-import { Avatar, Box, Flex, Text } from '@hackclub/design-system'
+import { Avatar, Flex, Text } from '@hackclub/design-system'
 import Link from 'gatsby-link'
 import React from 'react'
 
@@ -14,12 +14,12 @@ const Base = Flex.withComponent(Link).extend.attrs({ align: 'center' })`
     }
 `
 
-const UserCard = props => (
-    <Base to="/users/orpheus" {...props}>
-        <Avatar src="https://hackclub.com/team/orpheus.jpg" mr={2} />
+const UserCard = ({ name, username, avatarUrl, props }) => (
+    <Base to={`/users/${username}`} {...props}>
+        <Avatar src={avatarUrl} mr={2} />
         <Flex align="left" flexDirection="column">
-            <Name>Prophet Orpheus</Name>
-            <Text.span f={1} color="muted">@orpheus</Text.span>
+            <Name>{name}</Name>
+            <Text.span f={1} color="muted">@{username}</Text.span>
         </Flex>
     </Base>
 )
