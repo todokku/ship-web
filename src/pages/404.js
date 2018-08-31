abae14315611eb880d2e5ba349dad3d3630ec3c1
Victor Truong <me@ifvictr.com>
2018-08-30 20:31:18 -0700

    Remove extra code

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,13 @@
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome'
-import { Container, Flex, Heading, LargeButton, Text, cx } from '@hackclub/design-system'
+import { Flex, Heading, LargeButton, Text } from '@hackclub/design-system'
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
-import Footer from 'components/Footer'
 import Header from 'components/Header'
 
 const NotFound = () => (
     <Fragment>
         <Helmet title="404" />
         <Header color="muted" />
-        {/* <style children={`
-            body {
-                background: linear-gradient(
-                    8deg,
-                    ${cx('error')},
-                    ${cx('warning')}
-                );
-            }
-        `} /> */}
         <Flex align="center" justify="center" flexDirection="column" maxWidth={48} style={{ minHeight: '100vh' }}>
             <Heading.h1 color="primary" f={[5, 6]}>
                 404!
