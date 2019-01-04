85711f92428534cbd33c9f490cee858844888566
Victor Truong <me@ifvictr.com>
2019-01-03 16:45:31 -0800

    Add user page

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,10 +1,13 @@
+import { Avatar, Box, Container, Flex, Heading, Text, theme } from '@hackclub/design-system'
 import React, { Component, Fragment } from 'react'
 import Helmet from 'react-helmet'
 import Footer from 'components/Footer'
 import Header from 'components/Header'
 import LoadingBar from 'components/LoadingBar'
 import NotFound from 'components/NotFound'
+import ProjectCard from 'components/ProjectCard'
 import api from 'api'
+import { timeSince } from 'helpers'
 
 class Users extends Component {
     state = {
@@ -55,6 +58,39 @@ class Users extends Component {
                     <Fragment>
                         <Helmet title={user.name} />
                         <Header color="muted" />
+                        <Box bg="snow">
+                            <Container pt={6} pb={4} px={3} w={[1, null, 4 / 5]}>
+                                <Flex>
+                                    <Avatar src="https://github.com/ifvictr.png" size={128} />
+                                    {/* <Avatar src={user.avatar_url} size={128} /> */}
+                                    <Box ml={3}>
+                                        <Heading.h2 color="black">@{user.username}</Heading.h2>
+                                        <Text color="muted" mt={1}>Joined {timeSince(user.created_at)}</Text>
+                                    </Box>
+                                </Flex>
+                            </Container>
+                        </Box>
+                        <Box>
+                            <Container py={4} px={3} w={[1, null, 4 / 5]}>
+                                {projects.length > 0
+                                    ? (
+                                        <Flex style={{ margin: -theme.space[2] }} wrap>
+                                            {projects.map(project => (
+                                                <ProjectCard data={project} key={project.id} />
+                                            ))}
+                                        </Flex>
+                                    )
+                                    : status === 'projectsLoaded'
+                                        ? (
+                                            <Box bg="gray.1" align="center" my={4} p={3} style={{ borderRadius: theme.radius }}>
+                                                <Text color="black" f={3}>This user doesn’t appear to have shipped anything yet…</Text>
+                                            </Box>
+                                        )
+                                        : (
+                                            <LoadingBar />
+                                        )}
+                            </Container>
+                        </Box>
                         <Footer />
                     </Fragment>
                 )
